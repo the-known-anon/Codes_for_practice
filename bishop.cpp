f91bee44ce73a69de442379332cb4d01379ad3cd
the-known-anon <56070522+the-known-anon@users.noreply.github.com>
2020-12-03 16:17:14 +0530

    Create bishop.cpp

---
+++ bishop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+ using namespace std;
+ typedef long long ll;
+ 
+ int main(void){
+ll H,W;
+ cin >> H >> W;
+ 
+ if(H == 1 || W == 1){
+cout << 1 << endl;
+ } else {
+ cout << (H * W + 1) / 2 << endl;
+ }
+ return 0;
+ }
